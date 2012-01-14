a4de8eeedc89b7ac8f962e0d2144b4672094b4ca
David Ellis <d.f.ellis@ieee.org>
2012-01-13 16:18:52 -0800

    Added escaping for the system call, so non-contiguous strings (with spaces) are quoted, and quoted strings with quotes in them are escaped.

--- fakeShebang.c
+++ fakeShebang.c
@@ -2,6 +2,49 @@
 #include <string.h>
 #include <stdio.h>
 
+int escapedSize(const char* toEscape) {
+	int i, newSize = strlen(toEscape);
+	// Check if escaping is needed at all
+	if(strchr(toEscape, ' ') != NULL) {
+		newSize += 2;
+		// Check if we need to escape any quotes
+		if(strchr(toEscape, '"') != NULL) {
+			for(i = 0; i < strlen(toEscape); i++) {
+				if(*(toEscape+i) == '"') {
+					newSize++;
+				}
+			}
+		}
+	}
+	return newSize;
+}
+
+const char* escapeString(const char* toEscape) {
+	int i, escapedLength = escapedSize(toEscape);
+	char* escapedString;
+	if(strchr(toEscape, ' ') != NULL) {
+		escapedString = (char*)malloc((escapedLength+1)*sizeof(char));
+		*escapedString = '"';
+		*(escapedString+escapedLength) = '"';
+		if(strchr(toEscape, '"') != NULL) {
+			for(i = 0; i < escapedLength-2; i++) {
+				if(*(toEscape+i) == '"') {
+					*(escapedString+i) = '\\';
+					i++;
+					*(escapedString+i) = '"';
+				} else {
+					*(escapedString+i) = *(toEscape+i);
+				}
+			}
+		} else {
+			strncpy(escapedString+1, toEscape, strlen(toEscape));
+		}
+		return escapedString;
+	} else {
+		return toEscape;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	// Declare vars
 	const char *script = "./node_modules/test_script.js";
@@ -19,25 +62,25 @@ int main(int argc, char* argv[]) {
 
 	// Calculate the total sysCall string length and allocate
 	for(i = 1; i < argc; i++) {
-		sysCallLen += strlen(argv[i]) + 1;
+		sysCallLen += escapedSize(argv[i]) + 1;
 	}
-	sysCallLen += strlen(executable)+1+strlen(script);
+	sysCallLen += escapedSize(executable)+1+escapedSize(script);
 	sysCall = (char*)malloc(sysCallLen*sizeof(char));
 
 	// Copy the executable string, a space, and the script string, updating an offset along the way
-	strncpy(sysCall, executable, strlen(executable));
-	sysCallOffset += strlen(executable);
+	strncpy(sysCall, escapeString(executable), escapedSize(executable));
+	sysCallOffset += escapedSize(executable);
 	strncpy(sysCall+sysCallOffset, " ", 1);
 	sysCallOffset++;
-	strncpy(sysCall+sysCallOffset, script, strlen(script));
-	sysCallOffset += strlen(script);
+	strncpy(sysCall+sysCallOffset, escapeString(script), escapedSize(script));
+	sysCallOffset += escapedSize(script);
 
-	// Copy all arguments for the sysCall and shift the offset accordingly (TODO: Handle arguments that need escaping)
+	// Copy all arguments for the sysCall and shift the offset accordingly
 	for(i = 1; i < argc; i++) {
 		strncpy(sysCall+sysCallOffset, " ", 1);
 		sysCallOffset++;
-		strncpy(sysCall+sysCallOffset, argv[i], strlen(argv[i]));
-		sysCallOffset += strlen(argv[i]);
+		strncpy(sysCall+sysCallOffset, escapeString(argv[i]), escapedSize(argv[i]));
+		sysCallOffset += escapedSize(argv[i]);
 	}
 	
 	// Terminate the string
